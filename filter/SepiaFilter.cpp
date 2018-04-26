9b00de07546a74cca91b4a61f82b36074b73ead8
Daniel <dtoro@g.clemson.edu>
2018-04-26 15:37:39 -0400

    added sepia filter and removed input operator from pixel

--- filter/SepiaFilter.cpp
+++ filter/SepiaFilter.cpp
@@ -29,26 +29,26 @@ Pixel SepiaFilter::apply_transform(const Pixel& pix) const{
   red = red/255.0;
   blue = blue/255.0;
   green = green/255.0;
-  red = gamma_expansion(red);
-  blue = gamma_expansion(blue);
-  green = gamma_expansion(green);
+  red = Pixel::gamma_expansion(red);
+  blue = Pixel::gamma_expansion(blue);
+  green = Pixel::gamma_expansion(green);
   double outRed = ((red * 0.393) + (green * 0.769) + (blue * 0.189));
   double outGreen = ((red * 0.349) + (green * 0.686) + (blue * 0.168));
   double outBlue = ((red * 0.272) + (green * 0.534) + (blue * 0.131));
   if(outRed > 1.0){
-    red = inverse_gamma(1.0);
+    red = Pixel::inverse_gamma(1.0);
   } else {
-    red = inverse_gamme(outRed);
+    red = Pixel::inverse_gamme(outRed);
   };
   if(outGreen > 1.0){
-    green = inverse_gamma(1.0);
+    green = Pixel::inverse_gamma(1.0);
   } else {
-    green = inverse_gamme(outRed);
+    green = Pixel::inverse_gamme(outRed);
   };
   if(outBlue > 1.0){
-    blue = inverse_gamma(1.0);
+    blue = Pixel::inverse_gamma(1.0);
   } else {
-    blue = inverse_gamme(outRed);
+    blue = Pixel::inverse_gamme(outRed);
   };
   return Pixel(red, green, blue);
 
