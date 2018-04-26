349aad97bc6649a30dfcf33ff276d59b1bd9f823
Daniel <dtoro@g.clemson.edu>
2018-04-26 14:49:22 -0400

    added binary filter and input operator to pixel

--- filter/BinaryFilter.cpp
+++ filter/BinaryFilter.cpp
@@ -20,14 +20,14 @@ BinaryFilter::BinaryFilter(const Filter& f) :
 BinaryFilter::~BinaryFilter() {}
 
 Pixel BinaryFilter::apply_transform(const Pixel& pix) const{
-  Pixel a;
-  Pixel b;
+  // Pixel a;
+  // Pixel b;
   cout << "Enter RGB values for two pixels. Six numbers. Example: 120 45 200 100 100 100" << endl;
-  cin >> a >> b;
-  const Pixel c = a;
-  const Pixel d = b;
-  this->color1 = c;
-  this->color2 = d;
+  cin >> this->color1 >> this->color2;
+  // const Pixel c = a;
+  // const Pixel d = b;
+  // this->color1 = c;
+  // this->color2 = d;
   int lumin = pix.luminance();
   if(lumin > 255/2){
     return this->color1;
