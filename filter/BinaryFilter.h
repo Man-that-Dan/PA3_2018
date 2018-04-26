9a057a7119b1dae35984477c070e63970bb5e638
Daniel <dtoro@g.clemson.edu>
2018-04-26 15:13:44 -0400

    added binary filter and input operator to pixel

--- filter/BinaryFilter.h
+++ filter/BinaryFilter.h
@@ -6,36 +6,36 @@
 
 */
 
-#ifndef BINARYFILTER_H
-#define BINARYFILTER_H
-
-#include <string>
-#include <algorithm>
-#include "../image/Image.h"
-#include "PixelFilter.h"
-
-using namespace std;
-
-class BinaryFilter : public PixelFilter {
-  protected:
-
-    Pixel color1;
-    Pixel color2;
-
-    virtual Pixel apply_transform(const Pixel&) const;
-
-  public:
-    BinaryFilter();
-    BinaryFilter(string);
-    BinaryFilter(const Filter&);
-    ~BinaryFilter ();
-    void setcolors(Pixel&, Pixel&);
-
-
-    // Public Interface - every derived class must implement
-    //                  - or be abstract itself
-
-
-};
-
-#endif
+// #ifndef BINARYFILTER_H
+// #define BINARYFILTER_H
+//
+// #include <string>
+// #include <algorithm>
+// #include "../image/Image.h"
+// #include "PixelFilter.h"
+//
+// using namespace std;
+//
+// class BinaryFilter : public PixelFilter {
+//   protected:
+//
+//     Pixel color1;
+//     Pixel color2;
+//
+//     virtual Pixel apply_transform(const Pixel&) const;
+//
+//   public:
+//     BinaryFilter();
+//     BinaryFilter(string);
+//     BinaryFilter(const Filter&);
+//     ~BinaryFilter ();
+//     void setcolors(Pixel&, Pixel&);
+//
+//
+//     // Public Interface - every derived class must implement
+//     //                  - or be abstract itself
+//
+//
+// };
+//
+// #endif
